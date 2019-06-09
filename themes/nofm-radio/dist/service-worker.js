9bd8603a6fff7ca75588ac59a1573296735d9c08
Rizika <rizika@Rizikas-MacBook-Pro.local>
2019-06-09 12:03:57 -0500

    home ok

--- themes/nofm-radio/dist/service-worker.js
+++ themes/nofm-radio/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.946971ba396d8027e9ece45f73f7b204.js"
+  "precache-manifest.fc2b5b69370483f5e06aa193889407b8.js"
 );
 
 /**
