834d7f045d1a391c085782e0b86e76c1db5273df
Rizika <rizika@Rizikas-MacBook-Pro.local>
2019-06-13 19:33:25 -0500

    refactor: enqueuing files

---
+++ themes/nofm-radio/src/components/templates/single_archivo/index.js
@@ -0,0 +1,11 @@
+/*General Styles*/
+import '../../../css/style.css';
+
+/*Local Styles*/
+import './single-archivo-styles.css';
+import './single-archivo-scripts';
+
+/*Components*/
+import '../../parts/header/index';
+import '../../parts/footer/index';
+import '../../parts/sidebar/index';
