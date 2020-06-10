975002abcf29cf8d8b7ec66602ce23920fc0a274
HelaGone <luah613@gmail.com>
2020-06-09 19:39:58 -0500

    fix error in feed podcast

--- themes/nofm-radio/feeds/feed-podcast.php
+++ themes/nofm-radio/feeds/feed-podcast.php
@@ -21,7 +21,7 @@
 		'order'=>'DESC'
 	);
 
-	$p_cover 'http://nofm-radio.com/wp-content/uploads/2020/06/podcast.jpeg';
+	$p_cover = 'http://nofm-radio.com/wp-content/uploads/2020/06/podcast.jpeg';
 	$p_title = $show_title ? $show_title : get_bloginfo('name');
 	$p_description = $show_description ? $show_description : get_bloginfo('description');
 
